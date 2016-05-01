fcf13806be7e3b61b4cfaa6523a5bd5075b851da
TotoPower <TotoPower@TotoPower-PC>
2016-05-01 15:25:30 +0200

    1er commit

---
+++ src/main/java/net/persistance/db/tpjpa3/App.java
@@ -0,0 +1,20 @@
+package net.persistance.db.tpjpa3;
+
+import net.persistance.db.entities.ElectronicDevice;
+import net.persistance.db.entities.Heater;
+import net.persistance.db.entities.Home;
+import net.persistance.db.entities.Person;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+
+    }
+}
